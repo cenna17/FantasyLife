1de5398e01455a3555f68b8f41fe3ff64c34e8a7
cenna17 <itjustjenny10@gmail.com>
2025-09-12 08:03:44 +0000

    part1 and 2 done

---
+++ lab1/part2/script.sh
@@ -0,0 +1,9 @@
+
+
+gcc -Wall -Wpedantic -std=c17 -march=haswell -c array_sum.c -o array_sum.o
+gcc -Wall -Wpedantic -std=c17 -march=haswell -c array_sum_speed.c -o array_sum_speed.o
+
+gcc array_sum.o array_sum_speed.o 
+./a.out 1000000
+
+
---
+++ lab1/part3/script.sh
@@ -0,0 +1,7 @@
+as array_sum.S -o array_sum.o
+
+gcc -Wall -Wpedantic -std=c17 -march=haswell -c array_sum_speed.c -o array_sum_speed.o
+gcc array_sum.o array_sum_speed.o 
+./a.out
+
+
