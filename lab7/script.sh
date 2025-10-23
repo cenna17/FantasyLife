c0e40d12899185633e03ed5d8ac7ea4647ac3e7a
cenna17 <itjustjenny10@gmail.com>
2025-10-23 08:44:54 +0000

    lab 7 complete

--- lab7/script.sh
+++ lab7/script.sh
@@ -1,2 +1,4 @@
-gcc -Wall -Wpedantic -std=c17 -march=haswell -O3 tests.c lab7c.c lab7.S -o lab7
+gcc -Wall -Wpedantic -std=c17 -march=haswell -O3 tests.c lab7c.c lab7.S -o test
+gcc -Wall -Wpedantic -std=c17 -march=haswell -O3 lab7c.c timing.c lab7.S -o timing03
+gcc -Wall -Wpedantic -std=c17 -march=haswell lab7c.c timing.c lab7.S -o timing_reg
 
