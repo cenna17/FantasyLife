678c4b1d635419175877c0f556aa16a043cfd1ad
cenna17 <itjustjenny10@gmail.com>
2025-11-22 09:04:44 +0000

    lab11 complete

---
+++ lab11/script.sh
@@ -0,0 +1,3 @@
+g++ -O3 -mavx -std=c++17 lab11c.cpp lab10.S lab11_vc.cpp lab11.S tests.cpp -o lab11_test
+
+g++ -O3 -mavx -std=c++17 lab11c.cpp lab10.S lab11_vc.cpp lab11.S tests.cpp timing.cpp -o lab11_time
