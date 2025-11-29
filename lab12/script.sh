1cad065356fc69984d2d480fec729c27b62d978f
cenna17 <itjustjenny10@gmail.com>
2025-11-29 04:34:18 +0000

    lab12 completed

--- lab12/script.sh
+++ lab12/script.sh
@@ -1,11 +1,11 @@
-gcc -std=c17 -march=haswell -O0 lab12c.c -o run_o0
+gcc -std=c17 -march=haswell -O0 lab12c.c -o run_o0 && ./run_o0
 
-gcc -std=c17 -march=haswell -O1 lab12c.c -o run_o1
+gcc -std=c17 -march=haswell -O1 lab12c.c -o run_o1 && ./run_o1
 
-gcc -std=c17 -march=haswell -O2 lab12c.c -o run_o2
+gcc -std=c17 -march=haswell -O2 lab12c.c -o run_o2 && ./run_o2
 
-gcc -std=c17 -march=haswell -O3 lab12c.c -o run_o3
+gcc -std=c17 -march=haswell -O3 lab12c.c -o run_o3 && ./run_o3
 
 # build optimized executables (unsafe math)
-gcc -std=c17 -march=haswell -O3 -funsafe-math-optimizations lab12c.c -o map_o3_unsafe
-gcc -std=c17 -march=haswell -O3 -funsafe-math-optimizations lab12c.c -o dot_o3_unsafe
+gcc -std=c17 -march=haswell -O3 -funsafe-math-optimizations lab12c.c -o lab12c_o3_unsafe && ./lab12c_o3_unsafe
+gcc -std=c17 -march=haswell -funsafe-math-optimizations lab12c.c -o lab12c_unsafe && ./lab12c_unsafe
